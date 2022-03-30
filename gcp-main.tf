f01b2a1d9a792a820c8e1cf2658b03f6b8215733
Michael Abreu <michaellabreuu@hotmail.com>
2022-03-30 09:23:38 -0300

    ChangeLocation

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-cloud-6aso-g26"
-  location_id = "us-central"
+  location_id = "us-east"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-central1"
+  location = "us-east1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
